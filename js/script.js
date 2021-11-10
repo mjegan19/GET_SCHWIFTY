78166ee1e41a60a825f210b8dd0df5d72552a3c3
mjegan19 <87794144+mjegan19@users.noreply.github.com>
2021-11-10 23:51:59 +1100

    Implement refine search features

--- js/script.js
+++ js/script.js
@@ -1,33 +1,68 @@
 const searchCharacter = document.getElementById('searchCharacter');
 const searchButton = document.getElementById('searchButton');
+
+const refineResults = document.getElementById('refine-results');
+const gender = document.getElementById('gender');
+const species = document.getElementById('species');
+const lifeStatus = document.getElementById('status');
+
+const noResultsDiv = document.getElementById('no-results');
 const resultsDiv = document.getElementById('results');
 
 const pageNav = document.getElementById('page-nav');
 
 const nextPage = document.getElementById('next-page');
 const prevPage = document.getElementById('prev-page');
+
+
 let info = null;
+
 let character = "";
+
+let totalCharacters = null;
 let totalPages = null;
 let currentPage = 1;
 let cards = null;
 
 window.addEventListener("load", function () {
-  character = localStorage.getItem('enquiry')
+  character = localStorage.getItem('charName')
   searchCharacter.value = character;
+
+  getTotalCharacters();
+  searchMultiverse(currentPage, character, gender.value, species.value, lifeStatus.value);
 })
 
 searchButton.addEventListener('click', function () {
   character = searchCharacter.value;
-  resultsDiv.innerHTML = "";
-  currentPage = 1;
+  resetQueryString();
 
   if (totalPages > 1) {
     pageNav.classList.toggle('visually-hidden');
   }
 
-  localStorage.setItem("enquiry", character)
-  searchMultiverse(currentPage, character);
+  localStorage.setItem("charName", character)
+  searchMultiverse(currentPage, character, gender.value, species.value, lifeStatus.value);
+});
+
+// Event listener to refine gender of character query when changed
+gender.addEventListener('change', function () {
+  currentPage = 1;
+  searchMultiverse(currentPage, character, gender.value, species.value, lifeStatus.value);
+
+});
+
+// Event listener to refine species of character query when changed
+species.addEventListener('change', function () {
+  currentPage = 1;
+  searchMultiverse(currentPage, character, gender.value, species.value, lifeStatus.value);
+
+});
+
+// Event listener to refine life status of character query when changed
+lifeStatus.addEventListener('change', function () {
+  currentPage = 1;
+  searchMultiverse(currentPage, character, gender.value, species.value, lifeStatus.value);
+
 });
 
 prevPage.addEventListener('click', function () {
@@ -35,7 +70,7 @@ prevPage.addEventListener('click', function () {
     console.log('next');
     currentPage--
     pageNav.classList.toggle('visually-hidden');
-    searchMultiverse(currentPage, character);    
+    searchMultiverse(currentPage, character, gender.value, species.value, lifeStatus.value);    
     window.scroll({
       top: 0, 
       left: 0, 
@@ -49,7 +84,7 @@ nextPage.addEventListener('click', function () {
     console.log('next');
     currentPage++
     pageNav.classList.toggle('visually-hidden');
-    searchMultiverse(currentPage, character);
+    searchMultiverse(currentPage, character, gender.value, species.value, lifeStatus.value);
     window.scroll({
       top: 0, 
       left: 0, 
@@ -58,13 +93,14 @@ nextPage.addEventListener('click', function () {
   }
 });
 
-async function searchMultiverse(currentPage, enquiry) {
-  const response = await fetch(`https://rickandmortyapi.com/api/character?page=${currentPage}&name=${enquiry}`);
+async function searchMultiverse(currentPage, charName, genderType, speciesType, lifeStatus) {
+  const response = await fetch(`https://rickandmortyapi.com/api/character?page=${currentPage}&name=${charName}&gender=${genderType}&species=${speciesType}&status=${lifeStatus}`);
   const json = await response.json();
-  console.log(json);
+
   displayResults(json);
   info = json.info;
   totalPages = json.info.pages;
+  console.log('Multiverse ' + totalPages);
   pageNav.classList.toggle('visually-hidden');
 
   if (info.pages > 1) {
@@ -82,11 +118,10 @@ async function searchMultiverse(currentPage, enquiry) {
   } else {
     pageNav.classList.toggle('visually-hidden');
   }
-
-
 }
 
 function displayResults(data) {
+  noResultsDiv.innerHTML = "";
   resultsDiv.innerHTML = ""
 
   if (data.error) {
@@ -98,7 +133,7 @@ function displayResults(data) {
     </symbol>
   </svg>
   
-  <div class="alert alert-warning text-dark d-flex align-items-center m-5" role="alert">
+  <div class="alert alert-danger border-danger border-2 d-flex align-items-center" role="alert">
     <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
     <div>
       No results found! Please enter a different search query.
@@ -106,74 +141,86 @@ function displayResults(data) {
   </div>
     `;
 
-    resultsDiv.append(alertNone);
+    noResultsDiv.append(alertNone);
   
   } else {
 
-  data.results.forEach(characterAvatar => {
-    let newCard = document.createElement('div');
-    newCard.innerHTML = `
-    <div class="flip-card">
-    <!-- Front of Trading Card -->
-    <div class="flip-card-inner">
-      <div class="flip-card-front text-center">
-        <div class="flip-card-front-header">
-          <span id="set-list">${characterAvatar.id}/671</span><br />
-          <p id="char-name" class="char-name-header text-uppercase overflow-hidden">${characterAvatar.name}</p>
-        </div>
-        <img src="${characterAvatar.image}" alt="${characterAvatar.name}">
-        <div class="flip-card-front-footer">
-          <span>A U T O G R A P H</span>
-        </div>
-        <div class="flip-card-front-bottom">
-          <span>EYEHOLES - <em>THE OMNIVERSE CEREAL</em></span>
-        </div>
-      </div>      
-      <!-- Rear of Trading Card -->
-      <div class="flip-card-rear text-center">
-        <div class="flip-card-rear-bg">
-          <img src="./img/2872625-min.jpg" alt="Space Background Image for Card">
-          <div class="flip-card-rear-stats text-center">
-            <span>Rick & Morty Trading Cards</span><br />
-            <span class="char-name-header-rear text-uppercase overflow-hidden">${characterAvatar.name}</span><br />
-            <span class="text-info bg-dark stats-header">STATUS:</span><br />
-            <span class="stats-data lh-1 text-uppercase">${characterAvatar.status}</span><br />
-            <span class="text-info bg-dark stats-header">SPECIES:</span><br />
-            <span class="stats-data lh-1 text-uppercase">${characterAvatar.species}</span><br />
-            <span class="text-info bg-dark stats-header">GENDER:</span><br />
-            <span class="stats-data lh-1 text-uppercase">${characterAvatar.gender}</span><br />
-            <span class="text-info bg-dark stats-header">ORIGIN:</span><br />
-            <span class="stats-data lh-1 text-uppercase">${characterAvatar.origin.name}</span><br />
-            <span class="text-info bg-dark stats-header">LOCATION:</span><br />
-            <span class="stats-data lh-1 text-uppercase">${characterAvatar.location.name}</span><br />
-            <span class="text-info bg-dark stats-header">EPISODE APPEARANCES:</span><br />
-            <span class="stats-data lh-1 text-uppercase">${characterAvatar.episode.length}</span>
-            <div class="flip-card-rear-footer">
-              <p>Card ${characterAvatar.id} of 671<br />
-              Wubba Lubba Dub Dub</p>
+    data.results.forEach(characterAvatar => {
+      let newCard = document.createElement('div');
+      newCard.innerHTML = `
+      <div class="flip-card">
+      <!-- Front of Trading Card -->
+      <div class="flip-card-inner">
+        <div class="flip-card-front text-center">
+          <div class="flip-card-front-header">
+            <span id="set-list">${characterAvatar.id}/${totalCharacters}</span><br />
+            <p id="char-name" class="char-name-header text-uppercase overflow-hidden">${characterAvatar.name}</p>
+          </div>
+          <img src="${characterAvatar.image}" alt="${characterAvatar.name}">
+          <div class="flip-card-front-footer">
+            <span>A U T O G R A P H</span>
+          </div>
+          <div class="flip-card-front-bottom">
+            <span>EYEHOLES - <em>THE OMNIVERSE CEREAL</em></span>
+          </div>
+        </div>      
+        <!-- Rear of Trading Card -->
+        <div class="flip-card-rear text-center">
+          <div class="flip-card-rear-bg">
+            <img src="./img/2872625-min.jpg" alt="Space Background Image for Card">
+            <div class="flip-card-rear-stats text-center">
+              <span>Rick & Morty Trading Cards</span><br />
+              <span class="char-name-header-rear text-uppercase overflow-hidden">${characterAvatar.name}</span><br />
+              <span class="text-info bg-dark stats-header">STATUS:</span><br />
+              <span class="stats-data lh-1 text-uppercase">${characterAvatar.status}</span><br />
+              <span class="text-info bg-dark stats-header">SPECIES:</span><br />
+              <span class="stats-data lh-1 text-uppercase">${characterAvatar.species}</span><br />
+              <span class="text-info bg-dark stats-header">GENDER:</span><br />
+              <span class="stats-data lh-1 text-uppercase">${characterAvatar.gender}</span><br />
+              <span class="text-info bg-dark stats-header">ORIGIN:</span><br />
+              <span class="stats-data lh-1 text-uppercase">${characterAvatar.origin.name}</span><br />
+              <span class="text-info bg-dark stats-header">LOCATION:</span><br />
+              <span class="stats-data lh-1 text-uppercase">${characterAvatar.location.name}</span><br />
+              <span class="text-info bg-dark stats-header">EPISODE APPEARANCES:</span><br />
+              <span class="stats-data lh-1 text-uppercase">${characterAvatar.episode.length}</span>
+              <div class="flip-card-rear-footer">
+                <p>Card ${characterAvatar.id} of ${totalCharacters}<br />
+                Wubba Lubba Dub Dub</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-    `;
+      `;
 
-    newCard.className = "d-inline-block trading-card";
-    resultsDiv.append(newCard);
-  });
-
-  // Card flip click flip function - for loop creates array for all querySelectors' generated
-  var cards = document.querySelectorAll('.flip-card-inner');
-  for(let i = 0; i < cards.length; i++){
-    cards[i].addEventListener( 'click', function() {
-      cards[i].classList.toggle('is-flipped');
+      newCard.className = "d-inline-block trading-card";
+      resultsDiv.append(newCard);
     });
-  }
-  }
-  
+
+    // Card flip click flip function - for loop creates array for all querySelectors' generated
+    var cards = document.querySelectorAll('.flip-card-inner');
+    for(let i = 0; i < cards.length; i++){
+      cards[i].addEventListener('click', function() {
+        cards[i].classList.toggle('is-flipped');
+      });
+    }
+  }  
 }
 
-// function noResults() {
 
-// }
+async function getTotalCharacters() {
+  const response = await fetch(`https://rickandmortyapi.com/api/character`);
+  const json = await response.json();
+  totalCharacters = json.info.count;
+}
+
+function resetQueryString() {
+  resultsDiv.innerHTML = "";
+  noResultsDiv.innerHTML = "";
+  gender.value = "";
+  species.value = "";
+  lifeStatus.value = "";
+  refineResults.classList = "collapse";
+  currentPage = 1;
+}
