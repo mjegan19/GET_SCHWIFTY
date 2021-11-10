79bdd027bb563471a016dc12f84d5ce1edd23943
mjegan19 <87794144+mjegan19@users.noreply.github.com>
2021-11-10 14:24:55 +1100

    Added character NULL alert, compressed Img's

--- js/script.js
+++ js/script.js
@@ -30,7 +30,6 @@ searchButton.addEventListener('click', function () {
   searchMultiverse(currentPage, character);
 });
 
-
 prevPage.addEventListener('click', function () {
   if (info.prev) {
     console.log('next');
@@ -62,6 +61,7 @@ nextPage.addEventListener('click', function () {
 async function searchMultiverse(currentPage, enquiry) {
   const response = await fetch(`https://rickandmortyapi.com/api/character?page=${currentPage}&name=${enquiry}`);
   const json = await response.json();
+  console.log(json);
   displayResults(json);
   info = json.info;
   totalPages = json.info.pages;
@@ -88,6 +88,28 @@ async function searchMultiverse(currentPage, enquiry) {
 
 function displayResults(data) {
   resultsDiv.innerHTML = ""
+
+  if (data.error) {
+    let alertNone = document.createElement('div');
+    alertNone.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+    <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+      <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+    </symbol>
+  </svg>
+  
+  <div class="alert alert-warning text-dark d-flex align-items-center m-5" role="alert">
+    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+    <div>
+      No results found! Please enter a different search query.
+    </div>
+  </div>
+    `;
+
+    resultsDiv.append(alertNone);
+  
+  } else {
+
   data.results.forEach(characterAvatar => {
     let newCard = document.createElement('div');
     newCard.innerHTML = `
@@ -110,7 +132,7 @@ function displayResults(data) {
       <!-- Rear of Trading Card -->
       <div class="flip-card-rear text-center">
         <div class="flip-card-rear-bg">
-          <img src="./img/2872625.jpg" alt="Space Background Image for Card">
+          <img src="./img/2872625-min.jpg" alt="Space Background Image for Card">
           <div class="flip-card-rear-stats text-center">
             <span>Rick & Morty Trading Cards</span><br />
             <span class="char-name-header-rear text-uppercase overflow-hidden">${characterAvatar.name}</span><br />
@@ -148,6 +170,10 @@ function displayResults(data) {
       cards[i].classList.toggle('is-flipped');
     });
   }
+  }
   
 }
 
+// function noResults() {
+
+// }
